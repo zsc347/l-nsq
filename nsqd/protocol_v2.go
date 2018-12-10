a46c54e2131200846a47120d7bef3390fed3cbe9
zsc347 <zsc347@163.com>
2018-12-10 09:15:28 +0800

    protocal v2 1

--- nsqd/protocol_v2.go
+++ nsqd/protocol_v2.go
@@ -1,8 +1,14 @@
 package nsqd
 
 import (
+	"bytes"
 	"io"
 	"net"
+	"time"
+
+	"github.com/l-nsq/internal/protocol"
+
+	"github.com/l-nsq/internal/lg"
 )
 
 const (
@@ -26,4 +32,44 @@ func readMPUB(r io.Reader, tmp []byte, topic *Topic, maxMessageSize int64, maxBo
 
 func (p *protocolV2) IOLoop(conn net.Conn) error {
 	return nil
+}
+
+func (p *protocolV2) SendMessage(client *clientV2, msg *Message) error {
+	p.ctx.nsqd.logf(lg.DEBUG, "PROTOVAL(V2): writing msg(%s) to client (%s) - %s", msg.ID, client, msg.Body)
+	var buf = &bytes.Buffer{}
+
+	_, err := msg.WriteTo(buf)
+	if err != nil {
+		return err
+	}
+
+	err = p.Send(client, frameTypeMessage, buf.Bytes())
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p *protocolV2) Send(client *clientV2, frameType int32, data []byte) error {
+	client.writeLock.Lock()
+
+	var zeroTime time.Time
+	if client.HeartbeatInterval > 0 {
+		client.SetWriteDeadline(time.Now().Add(client.HeartbeatInterval))
+	} else {
+		client.SetWriteDeadline(zeroTime)
+	}
+
+	_, err := protocol.SendFramedResponse(client.Writer, frameType, data)
+	if err != nil {
+		client.writeLock.Unlock()
+		return err
+	}
+
+	if frameType != frameTypeMessage {
+		err = client.Flush()
+	}
+
+	client.writeLock.Unlock()
+	return err
 }
